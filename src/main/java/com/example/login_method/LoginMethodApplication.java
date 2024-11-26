6bcccdd52e0fe95d8c702740df971470992b016c
우상호 <clsrntkdgh@gmail.com>
2024-11-26 23:41:51 +0900

    설정

---
+++ src/main/java/com/example/login_method/LoginMethodApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_method;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginMethodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/login_method/LoginMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_method;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
