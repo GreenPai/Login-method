9d6c1c34ad1a65cca45e934ead9e21f27a513589
우상호 <clsrntkdgh@gmail.com>
2024-12-11 20:22:11 +0900

    설정

--- src/main/java/com/example/login_method/domain/member/MemberRepository.java
+++ src/main/java/com/example/login_method/domain/member/MemberRepository.java
@@ -1,17 +1,23 @@
 package com.example.login_method.domain.member;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Repository
-@RequiredArgsConstructor
+@Slf4j
 public class MemberRepository {
-    private static HashMap<Long, Member> store = new HashMap<>();
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
 
-
-
+    public Member save(Member member){
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
 
 }
--- src/main/java/com/example/login_method/web/member/MemberController.java
+++ src/main/java/com/example/login_method/web/member/MemberController.java
@@ -2,9 +2,14 @@ package com.example.login_method.web.member;
 
 import com.example.login_method.domain.member.Member;
 import com.example.login_method.domain.member.MemberRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -18,6 +23,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/members")
 public class MemberController {
     private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member){
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "members/addMemberForm";
+        }
+
+        memberRepository.save(member);
+        return "redirect:/";
+    }
 
 
 
